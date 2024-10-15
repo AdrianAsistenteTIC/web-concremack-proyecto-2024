59c791e02a0060699d185706ccd3f47d09fc7e72
LINO ADRIAN PAEZ REYES <asistente.tecnologia@pacolombia.com.co>
2024-10-15 11:21:22 -0500

    APR1991LINO
    OTROS

--- public/layout/session6.php
+++ public/layout/session6.php
@@ -5,14 +5,23 @@
   <section class="my-xl-9 my-5">
     <div class="container">
       <div class="row">
-        <div class="col-lg-6 offset-lg-3">
-          <div class="text-center mb-xl-7 mb-5">
-            <p class="mb-0 display-1">
-              TITULO
-            </p>
-            <p class="lead" style="text-align: justify;">
-              COMENTARIO.
-            </p>
+        <div class="col-lg-12 offset-lg-12">
+          <div class="text-left mb-xl-7 mb-5">
+            <div class="pattern-square"></div>
+            <section class="py-5 py-lg-12">
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-12">
+                    <h1 class="mb-3 display-1">
+                      TITULO
+                    </h1>
+                    <p class="mb-0" style="text-align: justify;">
+                      COMENTARIO.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
       </div>
@@ -30,14 +39,23 @@
   <section class="my-xl-9 my-5">
     <div class="container">
       <div class="row">
-        <div class="col-lg-6 offset-lg-3">
-          <div class="text-center mb-xl-7 mb-5">
-            <p class="mb-0 display-1">
-              TITULO
-            </p>
-            <p class="lead" style="text-align: justify;">
-              COMENTARIO.
-            </p>
+        <div class="col-lg-12 offset-lg-12">
+          <div class="text-left mb-xl-7 mb-5">
+            <div class="pattern-square"></div>
+            <section class="py-5 py-lg-12">
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-12">
+                    <h1 class="mb-3 display-1">
+                      TITULO
+                    </h1>
+                    <p class="mb-0" style="text-align: justify;">
+                      COMENTARIO.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
       </div>
